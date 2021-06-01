1e7b6e06efc2e6e767b7b2dacc14e98b5bcac7ef
MustafaCelal <celal.guler@ogr.sakarya.edu.tr>
2021-06-01 10:07:28 +0300

    Kitap ekleme view'ı ve actionı eklendi

---
+++ Controllers/BookController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookiesNSession.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult AddBook()
+        {
+            return View();
+        }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace CookiesNSession
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Book}/{action=AddBook}/{id?}");
             });
         }
     }
